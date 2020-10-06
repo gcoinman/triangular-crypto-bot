efc6357c5535a88ecea5b7885e56a4d2f819483a
Akira Yoshiyama <67716685+AkiraY1@users.noreply.github.com>
2020-10-05 22:34:28 -0700

    Update prices.py

--- prices.py
+++ prices.py
@@ -1,1 +1,9 @@
-import time
+import time, requests
+
+def print_all_pairs():
+  r = requests.get("https://api.binance.com/api/v3/ticker/price")
+  print(r.json())
+
+def find_price(pair):
+  r = requests.get(f"https://api.binance.com/api/v3/ticker/price?{pair}") #{'code': -1104, 'msg': "Not all sent parameters were read; read '0' parameter(s) but was sent '1'."}
+  print(r.json())
