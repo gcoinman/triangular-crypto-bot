32c9e890be6d6fbcd4ff02c4c57145693038a04f
Akira Yoshiyama <67716685+AkiraY1@users.noreply.github.com>
2020-10-06 07:41:58 -0700

    Worked on price_change function

--- prices.py
+++ prices.py
@@ -1,9 +1,23 @@
 import time, requests
 
 def print_all_pairs():
-  r = requests.get("https://api.binance.com/api/v3/ticker/price")
-  print(r.json())
+    r = requests.get("https://api.binance.com/api/v3/ticker/price")
+    print(r.json())
 
-def find_price(pair):
-  r = requests.get(f"https://api.binance.com/api/v3/ticker/price?{pair}") #{'code': -1104, 'msg': "Not all sent parameters were read; read '0' parameter(s) but was sent '1'."}
-  print(r.json())
+def price(pair):
+    r = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={pair}")
+    return float(r.json()['price'])
+
+def price_change(pair):
+    p = price(pair)
+    print(p)
+    while True:
+        num = price(pair)
+        if num != p:
+            r = num-p
+            change = f"{r:.9f}"
+            print(f"Price: {num} | Change: {change}")
+            p = num
+    return ""
+
+price_change("LTCBTC")
